f2dcaa1641ebaa9b82c2c2f8177244b867c1ce6a
milyafajor <alfajor.mily@gmail.com>
2025-08-08 01:49:29 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'mily-cache-v1';
+const assetsToCache = [
+  '.',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
